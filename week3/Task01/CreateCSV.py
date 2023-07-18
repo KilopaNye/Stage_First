f2598e9f96ddd24128a0ac6320ad08a95c91cfe4
Kilopa Nye <steve69988@gmail.com>
2023-07-18 16:51:56 +0800

    第三周作業7/18版本上傳

--- week3/Task01/CreateCSV.py
+++ week3/Task01/CreateCSV.py
@@ -1,23 +1,30 @@
 import json, urllib.request
 import csv
+
 url = "https://padax.github.io/taipei-day-trip-resources/taipei-attractions-assignment.json"
-#----------------------- attraction資料抓取並寫入-------------------------
-with open("attraction.csv",mode="w", newline="", encoding='utf-8') as attraction:
-    writer=csv.writer(attraction)
+# ----------------------- attraction資料抓取並寫入-------------------------
+with open("attraction.csv", mode="w", newline="", encoding="utf-8") as attraction:
+    writer = csv.writer(attraction)
     with urllib.request.urlopen(url) as json_data:
         # 將JSON進行UTF-8的BOM解碼，並把解碼後的資料載入JSON陣列中
         data = json.loads(json_data.read().decode("utf-8-sig"))
     # 指定內文為result
-    result=data["result"]["results"]
+    result = data["result"]["results"]
     for i in range(len(result)):
-        #篩選出第一筆圖片網址
+        # 篩選出第一筆圖片網址
         first_url = result[i]["file"].split("https://")
         # 景點名稱,區域,經度,緯度,第⼀張圖檔網址
-        write_in=result[i]["stitle"],result[i]["address"][4:8],result[i]["longitude"],result[i]["latitude"],"https://"+first_url[1]
-        #寫入資料
+        write_in = (
+            result[i]["stitle"],
+            result[i]["address"][4:8],
+            result[i]["longitude"],
+            result[i]["latitude"],
+            "https://" + first_url[1],
+        )
+        # 寫入資料
         writer.writerow(write_in)
 
-#------------------------MRT所需資料製作-------------------------------------
+# ------------------------MRT所需資料製作-------------------------------------
 with open("mrt.csv", mode="w", newline="", encoding="utf-8") as mrt:
     writer = csv.writer(mrt)
     with urllib.request.urlopen(url) as json_data:
@@ -30,7 +37,7 @@ with open("mrt.csv", mode="w", newline="", encoding="utf-8") as mrt:
         # 寫入資料
         writer.writerow(write_in)
 
-#------------------------MRT讀取資料整理並寫入------------------------------
+# ------------------------MRT讀取資料整理並寫入------------------------------
 with open("mrt.csv", mode="r", newline="", encoding="utf-8") as mrt:
     writer = csv.reader(mrt)
     writer = list(writer)
@@ -38,13 +45,13 @@ with open("mrt.csv", mode="r", newline="", encoding="utf-8") as mrt:
     with open("mrt.csv", mode="w", newline="", encoding="utf-8") as mrt:
         data = csv.writer(mrt)
         answer = ()
-        #這邊使用tuple的+=功能進行長度增加 假如站名與前一個不相符則加入站名title 反之增加景點名稱至tuple中 
+        # 這邊使用tuple的+=功能進行長度增加 假如站名與前一個不相符則加入站名title 反之增加景點名稱至tuple中
         # 假如再執行一次加入站名title的功能，則先印入資料一次當作斷點，並繼續下一站的輪播。以此類推
         for i in range(len(writer)):
-            #將沒有捷運站的景點剔除
+            # 將沒有捷運站的景點剔除
             if bool(writer[i][0]) != False:
                 if writer[i][0] != writer[i - 1][0]:
-                    if len(answer)>=1:
+                    if len(answer) >= 1:
                         data.writerow(answer)
                     answer = writer[i][0], writer[i][1]
                 else:
---
+++ week3/Task03/js.js
@@ -0,0 +1,108 @@
+//選單的縮放，使用hide的方法
+function hiden() {
+    let hide = document.querySelector(".hide");
+    let nav = document.querySelector(".menuIcon");
+    if (hide.style.display == "none" || hide.style.display == "") { //加上或為空值，解決要按兩次才執行函式的問題
+        hide.style.display = "block";
+        nav.style.display = "none";
+    } else {
+        hide.style.display = "none";
+        nav.style.display = "block";
+    };
+};
+
+/*改善放大螢幕寬度時漢堡圖還存在的問題*/
+window.onload = function () {//當載入網頁後執行函式
+    function resize() {
+        let nav = document.querySelector(".menuIcon");
+        if (window.innerWidth > 600) {   //偵測到螢幕寬度大於600px後運行函式
+            nav.style.display = "none";
+        } else {
+            nav.style.display = "block";  //小於600px記得要恢復block設定
+        };
+    };
+    resize()
+    window.addEventListener('resize', resize, false);	// 持續偵聽事件 resize
+};
+
+// let star = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+// function gray(number) {
+//     let gray = document.querySelector(".gray-" + number);/*選取要操作的標籤，從0開始*/
+//     if (star[number] == 0) {/*判定star的值是不是0*/
+//         gray.style.filter = "grayscale(0)";//操作gray的classStyle
+//         star[number] += 1;
+//     } else {
+//         gray.style.filter = "grayscale(1)";
+//         star[number] = 0;
+//     };
+// };
+
+
+fetch("https://padax.github.io/taipei-day-trip-resources/taipei-attractions-assignment.json").then(function (response) {
+    return response.json();
+}).then(function (data) {
+    // 前三個方框內容生成
+    let subs;
+    for (let j = 1; j < 4; j++) {
+        let first = document.querySelector(".box" + j);
+        // 創建fragment 虛擬DOM 不影響現有原生文件
+        let fragment = document.createDocumentFragment();
+
+        // 照片區塊、以及整理網址
+        let httpConnect = String(data["result"]["results"][j]["file"]);
+        subs = httpConnect.split(".jpg");  //分割網址
+        let img = document.createElement("img");
+        img.src = subs[0] + ".jpg";  //新增圖片網址
+        img.classList.add("img");  //新增圖片class
+        // 先將東西加到fragment階層，後面再一次把全部的fragment加到first階層中。
+        fragment.appendChild(img);
+
+        // 文字方塊生成
+        let div = document.createElement("div");
+        div.classList.add("photoTxt");
+        div.appendChild(document.createTextNode(data["result"]["results"][j]["stitle"]));
+        fragment.appendChild(div);
+        first.appendChild(fragment);
+
+    };
+    for (let i = 4; i < 16; i++) {
+        let second = document.querySelector(".box" + i);
+        let fragmentSecond = document.createDocumentFragment();
+
+        // 照片區塊、以及整理網址
+        let httpConnect = String(data["result"]["results"][i]["file"]);
+        // 因為有結尾是大寫JPG，因此使用判斷式進行篩選
+        if (i == 12) {
+            subs = httpConnect.split(".JPG");  //分割網址
+            let img2 = document.createElement("img");
+            img2.src = subs[0] + ".JPG";
+            img2.classList.add("img2");  //新增圖片class
+            // 先將東西加到fragmentSecond階層，後面再一次把全部的fragmentSecond加到second階層中。
+            fragmentSecond.appendChild(img2);
+            second.appendChild(fragmentSecond);
+        } else {
+            subs = httpConnect.split(".jpg");  //分割網址
+            let img2 = document.createElement("img");
+            img2.src = subs[0] + ".jpg";
+            img2.classList.add("img2");  //新增圖片class
+            // 先將東西加到fragmentSecond階層，後面再一次把全部的fragmentSecond加到second階層中。
+            fragmentSecond.appendChild(img2);
+            second.appendChild(fragmentSecond);
+        };
+        //文字
+        let secondText = document.querySelector(".num" + i);
+        let div = document.createElement("div");
+        div.classList.add("photoTxt2");
+        div.appendChild(document.createTextNode(data["result"]["results"][i]["stitle"]));
+        fragmentSecond.appendChild(div);
+        secondText.appendChild(fragmentSecond);
+    };
+});
+
+// 參考框架
+    // <div class="secondPhoto" >
+    //         <img src="image/food.jpg" class="img2">
+    //         <div class="opacity">
+    //             <div class="photoTxt2">Title 1</div>
+    //         </div>
+    //     </div>
