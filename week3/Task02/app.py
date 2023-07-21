93115671a2e8273b6ecaa170d594acd8b5530945
KilopaNye <98875404+KilopaNye@users.noreply.github.com>
2023-07-21 13:15:51 +0800

    Update app.py

--- week3/Task02/app.py
+++ week3/Task02/app.py
@@ -35,16 +35,17 @@ def getPage(url):
         titles = root.find_all("div", class_="title")
         nums = root.find_all("div", class_="nrec")
         for i in range(len(titles)):
+            #排除網址為公告版的文章title，以及文章已刪除的文章標題
             if titles[i].a != None and titles[i].a["href"] != "/bbs/movie/M.1630756788.A.1FE.html":
                 Sharetime = getTime("https://www.ptt.cc"+titles[i].a["href"])
-                
+                ＃將推文數為空值的數值顯示為０
                 if nums[i].span ==None:
                     result= titles[i].a.string,"0",Sharetime
                     writer.writerow(result)
                 else:
                     result = titles[i].a.string,nums[i].span.string,Sharetime
                     writer.writerow(result)
-
+        ＃找到上一頁的按鈕並且回傳值
         nextLink = root.find("a", string="‹ 上頁")
         return nextLink["href"]
 
