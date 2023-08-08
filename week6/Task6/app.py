6b45472f97125053b16e37c7499052983d195861
Kilopa Nye <steve69988@gmail.com>
2023-08-08 16:42:13 +0800

    第六周進階題提交

---
+++ week6/Task6/app.py
@@ -0,0 +1,116 @@
+from flask import Flask
+from flask import request
+from flask import render_template
+from flask import redirect
+from flask import session
+from flask import jsonify
+import mysql.connector
+
+
+app = Flask(__name__, static_folder="public", static_url_path="/")
+app.secret_key = "WGXaTKE7JR9MzzykHVp1O8ix7cnkx5eOb400I5gPxXJI3I8saAUWZjDLxs6056M"
+
+con = mysql.connector.connect(
+    user="root", password="root123", host="localhost", database="website"
+)
+cursor = con.cursor(dictionary=True)
+# 首頁
+@app.route("/")
+def home():
+    return render_template("public.html")
+
+
+# 註冊帳號
+@app.route("/signup", methods=["POST"])
+def signup():
+    # 連線到資料庫
+    count_name = request.form["count_name"]
+    user_count = request.form["user_count"]
+    password = request.form["password"]
+    username = (user_count,)  # 將資料轉為tuple才可被SQL語法操作
+    cursor.execute("select username from member where username = %s", (username))
+    data = cursor.fetchone()
+    if data:
+        return redirect("/error?message=帳號已經被註冊")
+    else:
+        cursor.execute(
+            "insert into member(name, username, password) values(%s, %s, %s )",(count_name, user_count, password))
+        con.commit()
+        cursor.execute("select * from member where username=%s", (username))
+        data = cursor.fetchone()
+        session["username"] = count_name
+        session["id"] = data["name"]
+        return redirect("/member")
+
+
+# 登入用表單，並且驗證
+@app.route("/signin", methods=["POST"])
+def signin():
+    count = request.form["count"]
+    password = request.form["password"]
+    # username=(count,) #將資料轉為tuple才可被SQL語法操作
+    cursor.execute(
+        "select id,name,username,password from member where username = %s and password = %s ",(count, password))
+    data = cursor.fetchone()
+    if data:
+        print(data["id"])
+        session["id"] = data["id"]
+        session["username"] = data["username"]
+        session["name"] = data["name"]
+        return redirect("/member")
+    else:
+        return redirect("/error?message=帳號、或密碼輸入錯誤")
+
+
+@app.route("/createMessage", methods=["POST"])
+def createMessage():
+    message = request.form["message"]
+    if "username" in session:
+        input_message = message
+        id = session["id"]
+        cursor.execute(
+            "insert into message(member_id, content) values(%s, %s )",
+            (id, input_message),
+        )
+        con.commit()
+        return redirect("/member")
+    else:
+        return redirect("/")
+
+
+# 先驗證是否有先登入才導至會員頁面
+@app.route("/member")
+def member():
+    if "username" in session:
+        cursor.execute("select message.id,name,content,message.time from member inner join message on member.id=message.member_id order by message.time desc")
+        data = cursor.fetchall()
+        print(data)
+        username = session["name"]
+        return render_template("member.html", username=username, data=data)
+    else:
+        return redirect("/")
+    
+@app.route("/deleteMessage", methods=["POST"])
+def deleteMessage():
+    data=request.get_json()
+    text=data.get("text")
+    text=(text,)
+    cursor.execute("delete from message where id=%s",(text))
+    con.commit()
+    return jsonify({"message": "資料刪除成功"})
+
+# 錯誤頁面導向
+@app.route("/error")
+def error():
+    message = request.args.get("message", "發生意外的錯誤")
+    return render_template("error.html", message=message)
+
+# 登出
+@app.route("/signout")
+def signout():
+    # 移除密鑰
+    session.clear()
+    return redirect("/")
+
+
+app.run(port=3000, debug=True)
---
+++ week6/Task6/public/submit.js
@@ -0,0 +1,44 @@
+function GoForm() {
+    document.querySelector(".signup-form").onsubmit = function (event) {
+        let count_name = document.querySelector("#count_name").value;
+        let user_count = document.querySelector("#user_count").value;
+        let password = document.querySelector("#password").value;
+        console.log(count_name)
+        if (count_name.length < 1 || user_count.length < 1 || password.length < 1) {
+            alert("不得為空，請正確填寫表單");
+            event.preventDefault(); // 阻止表單提交
+        };
+    };
+};
+
+function signin() {
+    document.querySelector(".login-form").onsubmit = function (event) {
+        let user_count = document.querySelector("#login-count").value;
+        let password = document.querySelector("#login-password").value;
+        console.log(count_name)
+        if (user_count < 1 || password < 1) {
+            alert("不得為空，請正確填寫表單");
+            event.preventDefault(); // 阻止表單提交
+        };
+    };
+};
+
+function delMessage(button) {
+    let OK = confirm("確定要刪除嗎?")
+    if (OK) {
+        let paragraph = button.parentNode;
+        let textContent = paragraph.textContent.trim();
+        let textCut = textContent.substring(0, textContent.length - 1); //去掉按鈕中的"X"
+        console.log("要刪除的文字：" + textCut);
+        fetch("/deleteMessage", {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({ text: textCut })
+        }).then(response => response.json()).then(data => {
+            console.log("刪除成功", data);
+            window.location.href = "/member";
+        }).catch(error => {
+            console.error("發生錯誤", error);
+        });
+    }
+}
